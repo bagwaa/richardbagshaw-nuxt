20a70c0d2b3e4d18d46ce74438cd2e07866eb14e
Richard Bagshaw <me@richardbagshaw.co.uk>
2020-02-08 15:03:35 +0000

    add a default slug page for blog posts

---
+++ pages/_slug.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3 class="my-4">> {{ slug }}</h3>
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: 'Richard Bagshaw - About Me',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'About Me | Richard Bagshaw Freelance Web Developer'
+        }
+      ]
+    }
+  },
+  computed: {
+    slug() {
+      return this.$route.params.slug
+    }
+  }
+}
+</script>
